b233b51ec449f591c1fb82e9d2bb4b18c6f1908d
LingoNMBU <erlinge@nmbu.no>
2022-11-06 21:00:18 +0100

    hyperband changes

--- ca2orion4.py
+++ ca2orion4.py
@@ -47,6 +47,25 @@ def f1_m(y_true, y_pred):
     recall = recall_m(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
+
+def conv2d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
+    """Function to add 2 convolutional layers with the parameters passed to it"""
+    # first layer
+    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size),\
+              kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    # second layer
+    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size),\
+              kernel_initializer = 'he_normal', padding = 'same')(x)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    return x
+
 from tensorflow.keras.applications import VGG16
 from keras.models import Model
 
