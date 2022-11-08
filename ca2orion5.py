f3865282afea3d89452246969e5294acfd6b8ad5
LingoNMBU <erlinge@nmbu.no>
2022-11-08 21:34:20 +0100

    unet 2

--- ca2orion5.py
+++ ca2orion5.py
@@ -153,9 +153,8 @@ class CVTuner(keras_tuner.engine.tuner.Tuner):
     
 
 tuner = CVTuner(hypermodel=build_tuning_model,
-                oracle=keras_tuner.oracles.BayesianOptimization(objective='val_loss', 
-                                                               max_trials=40,
-                                                               num_initial_points=None))
+                oracle=keras_tuner.oracles.HyperbandOracle((objective='val_loss', 
+                                                               max_epochs=50,)
 tuner.search_space_summary()
 x, y = X_train, y_train  # NumPy data
 tuner.search(x, y, batch_size=64, epochs=30, overwrite=True)
